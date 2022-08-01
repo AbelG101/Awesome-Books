36b2791bb702bfa3db7e9fe146c34a93d81a9c2d
Surafel Getachew <suragetch@gmail.com>
2022-08-01 18:16:17 +0300

    Add saveon local sotrage method

--- script.js
+++ script.js
@@ -12,3 +12,44 @@ window.onload = () => {
     });
   }
 };
+
+function saveOnLocalStorage(book) {
+    books = JSON.parse(localStorage.getItem('All Books: '));
+    // if local storage is empty
+    if (books == null) books = [];
+    books.push(book);
+    localStorage.setItem('Book: ', JSON.stringify(book));
+    localStorage.setItem('All Books: ', JSON.stringify(books));
+  }
+  
+  function addBookOnPage(book) {
+    const div = document.createElement('div');
+    const authorParag = document.createElement('p');
+    const titleParag = document.createElement('p');
+    const btn = document.createElement('button');
+    const hr = document.createElement('hr');
+    authorParag.textContent = book.author;
+    titleParag.textContent = book.title;
+    btn.textContent = 'remove';
+    btn.setAttribute('class', 'remove-btn');
+    div.setAttribute('class', 'book');
+    div.appendChild(titleParag);
+    div.appendChild(authorParag);
+    div.appendChild(btn);
+    div.appendChild(hr);
+    booksContainer.appendChild(div);
+  }
+  
+  function addBook(author, title) {
+    let book = {
+      title: String,
+      author: String
+    };
+  
+    book.title = title.value;
+    book.author = author.value;
+  
+    saveOnLocalStorage(book);
+    addBookOnPage(book);
+  }
+  
