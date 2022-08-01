8631ff459b09d8052fccfe36d61e420e6af71f66
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-01 16:07:17 -0700

    modify the script for new styling

--- script.js
+++ script.js
@@ -11,20 +11,17 @@ function saveOnLocalStorage(book) {
 }
 function addBookOnPage(book) {
   const div = document.createElement('div');
-  const authorParag = document.createElement('p');
-  const titleParag = document.createElement('p');
-  const btn = document.createElement('button');
-  const hr = document.createElement('hr');
-  authorParag.textContent = book.author;
-  titleParag.textContent = book.title;
-  btn.textContent = 'remove';
-  btn.setAttribute('class', 'remove-btn');
+  const authorTitle = document.createElement('p');
+  const removeBtn = document.createElement('button');
+  authorTitle.textContent = '"' + book.title + '"' + ' by ' + book.author;
+  removeBtn.textContent = 'Remove';
+  removeBtn.setAttribute('class', 'remove-btn');
   div.setAttribute('class', 'book');
-  div.appendChild(titleParag);
-  div.appendChild(authorParag);
-  div.appendChild(btn);
-  div.appendChild(hr);
+  div.appendChild(authorTitle);
+  div.appendChild(removeBtn);
   booksContainer.appendChild(div);
+  if (booksContainer.childNodes.length !== 0)
+    booksContainer.classList.add('active');
 }
 window.onload = () => {
   books = JSON.parse(localStorage.getItem('All Books: '));
@@ -49,6 +46,8 @@ function removeBook(index) {
   books.splice(index, 1);
   booksDiv[index].remove();
   localStorage.setItem('All Books: ', JSON.stringify(books));
+  if (booksContainer.childNodes.length === 1)
+    booksContainer.classList.toggle('active');
 }
 document.addEventListener('click', (e) => {
   if (!e.target.matches('.remove-btn')) {
