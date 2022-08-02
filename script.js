83247480aec8ef7b0f600bcf7e935c2c9310ffe9
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-02 12:06:55 -0700

    removed BookOperations class and add static methods for BookClass
    fix linter errors

--- script.js
+++ script.js
@@ -1,59 +1,61 @@
 const addBtn = document.querySelector('.add-btn');
 const booksContainer = document.querySelector('.books-container');
-
-class BookClass {
-  constructor(title, author) {
-    this.author = author;
-    this.title = title;
-  }
-};
-
-class BookOperations {
-  constructor() {
-    this.books = [];
-  }
-  addBook(title, author) {
-    const book = new BookClass(title, author)
-    saveOnLocalStorage(book);
-    addBookOnPage(book);
-  }
-  removeBook(index) {
-    const booksDiv = document.querySelectorAll('.book');
-    this.books.splice(index, 1);
-    booksDiv[index].remove();
-    localStorage.setItem('All Books: ', JSON.stringify(this.books));
-    if (booksContainer.childNodes.length === 1)
-      booksContainer.classList.toggle('active');
-  }
-};
-
-const bookOperations = new BookOperations();
-
 function saveOnLocalStorage(book) {
-  bookOperations.books = JSON.parse(localStorage.getItem('All Books: '));
-  if (bookOperations.books == null) bookOperations.books = [];
-  bookOperations.books.push(book);
+  BookClass.books = JSON.parse(localStorage.getItem('All Books: '));
+  if (BookClass.books == null) BookClass.books = [];
+  BookClass.books.push(book);
   localStorage.setItem('Book: ', JSON.stringify(book));
-  localStorage.setItem('All Books: ', JSON.stringify(bookOperations.books));
+  localStorage.setItem('All Books: ', JSON.stringify(BookClass.books));
 }
 function addBookOnPage(book) {
   const div = document.createElement('div');
   const authorTitle = document.createElement('p');
   const removeBtn = document.createElement('button');
-  authorTitle.textContent = '"' + book.title + '"' + ' by ' + book.author;
+  authorTitle.textContent = `"${book.title}" by ${book.author}`;
   removeBtn.textContent = 'Remove';
   removeBtn.setAttribute('class', 'remove-btn');
   div.setAttribute('class', 'book');
   div.appendChild(authorTitle);
   div.appendChild(removeBtn);
   booksContainer.appendChild(div);
-  if (booksContainer.childNodes.length !== 0)
+  if (booksContainer.childNodes.length !== 0) {
     booksContainer.classList.add('active');
+  }
 }
+class BookClass {
+  constructor(title, author) {
+    this.author = author;
+    this.title = title;
+  }
+
+  static createArray() {
+    this.books = [];
+  }
+
+  static addBook(title, author) {
+    const book = new BookClass(title, author);
+    saveOnLocalStorage(book);
+    addBookOnPage(book);
+  }
+
+  static removeBook(index) {
+    const booksDiv = document.querySelectorAll('.book');
+    this.books.splice(index, 1);
+    booksDiv[index].remove();
+    localStorage.setItem('All Books: ', JSON.stringify(this.books));
+    if (booksContainer.childNodes.length === 1) {
+      booksContainer.classList.toggle('active');
+    }
+  }
+
+}
+
+BookClass.createArray();
+
 window.onload = () => {
-  bookOperations.books = JSON.parse(localStorage.getItem('All Books: '));
-  if (bookOperations.books !== null) {
-    bookOperations.books.forEach((book) => {
+  BookClass.books = JSON.parse(localStorage.getItem('All Books: '));
+  if (BookClass.books !== null) {
+    BookClass.books.forEach((book) => {
       addBookOnPage(book);
     });
   }
@@ -65,12 +67,12 @@ document.addEventListener('click', (e) => {
   const removeBtns = document.querySelectorAll('.remove-btn');
   removeBtns.forEach((removeBtn, index) => {
     if (e.target === removeBtn) {
-      bookOperations.removeBook(index);
+      BookClass.removeBook(index);
     }
   });
 });
 addBtn.addEventListener('click', () => {
   const author = document.getElementById('author').value;
   const title = document.getElementById('title').value;
-  bookOperations.addBook(title, author);
+  BookClass.addBook(title, author);
 });
