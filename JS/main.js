ac5de096466596869cecaccb03e82ba5cb6f8f35
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-03 12:25:58 -0700

    fix linter errors
    removed books container from add section

--- JS/main.js
+++ JS/main.js
@@ -1,29 +1,22 @@
-import {
-	callRemoveBook,
-	callAddBook,
-	retrieveBookData
-} from './bookScript.js';
-import {
-	app
-} from './singlePageApp.js'
+import { callRemoveBook, callAddBook, retrieveBookData } from './bookScript.js';
+import { app } from './singlePageApp.js'
 
 const addBtn = document.querySelector('.add-btn');
-
 function onPageLoad() {
-	retrieveBookData();
-	app.init();
+  retrieveBookData();
+  app.init();
 }
 window.onload = onPageLoad();
 document.addEventListener('click', (e) => {
-	if (!e.target.matches('.remove-btn')) {
-		return;
-	}
-	const removeBtns = document.querySelectorAll('.remove-btn');
-	removeBtns.forEach((removeBtn, index) => {
-		if (e.target === removeBtn) {
-			callRemoveBook(e.target.parentElement.getAttribute('id'));
-		}
-	});
+  if (!e.target.matches('.remove-btn')) {
+    return;
+  }
+  const removeBtns = document.querySelectorAll('.remove-btn');
+  removeBtns.forEach((removeBtn, index) => {
+    if (e.target === removeBtn) {
+      callRemoveBook(e.target.parentElement.getAttribute('id'));
+    }
+  });
 
 });
 addBtn.addEventListener('click', callAddBook);
--- JS/singlePageApp.js
+++ JS/singlePageApp.js
@@ -1,20 +1,18 @@
 const app = {
-	init: function() {
-		console.log("currentPage");
-		document.querySelectorAll('.nav-link').forEach((link) => {
-			link.addEventListener('click', app.nav);
-		})
-		history.replaceState({}, 'Home', '#home');
-	},
-	nav: function(ev) {
-		ev.preventDefault();
-		let currentPage = ev.target.getAttribute('data-target');
+  init: function() {
+    console.log("currentPage");
+    document.querySelectorAll('.nav-link').forEach((link) => {
+      link.addEventListener('click', app.nav);
+    })
+    history.replaceState({}, 'Home', '#home');
+  },
+  nav: function(ev) {
+    ev.preventDefault();
+    let currentPage = ev.target.getAttribute('data-target');
 
-		document.querySelector('.active').classList.remove('active');
-		document.getElementById(currentPage).classList.add('active');
-		history.pushState({}, currentPage, `#${currentPage}`);
-	},
+    document.querySelector('.active').classList.remove('active');
+    document.getElementById(currentPage).classList.add('active');
+    history.pushState({}, currentPage, `#${currentPage}`);
+  },
 }
-export {
-	app
-};
+export { app };
