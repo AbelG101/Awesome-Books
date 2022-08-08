a9b3b4d943af0f0e7b784d1b51692ce1413ae7de
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-08 13:52:02 +0300

    fix linter errors

--- modules/DateTimeScript.js
+++ modules/DateTimeScript.js
@@ -1,14 +1,13 @@
-import { DateTime } from '../node_modules/luxon/build/es6/luxon.js';
+import { DateTime } from '../LuxonFiles/luxon/build/es6/luxon.js';
+
 function getDateTime() {
   const dateTimeDiv = document.querySelectorAll('.date-time');
- 
-  setInterval(() => {
+   setInterval(() => {
     const now = DateTime.local();
     const nowFormatted = now.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
-    console.log(nowFormatted)
     dateTimeDiv.forEach((elt) => {
       elt.textContent = nowFormatted;
-    })
-  }, 1000)
+    });
+  }, 1000);
 }
 export default getDateTime;
